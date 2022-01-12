9a5ff4a5b02eb7344ea7cccfccd25e4f6ed10433
Herbert Nícolas <hna@cin.ufpe.br>
2022-01-12 13:17:41 -0300

    topbar icons aligned to the right. changed icons database to react-icons

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,26 +1,24 @@
 import "./topbar.scss"
-import {MailIcon} from '@mui/icons-material/Mail';      //ERROR in ./src/components/topbar/Topbar.jsx 25:42-50
-import {BookIcon} from '@mui/icons-material/Book';     //ERROR in ./src/components/topbar/Topbar.jsx 25:42-50
+
+import {BsJournalBookmarkFill, BsWhatsapp} from "react-icons/bs";      //ERROR in ./src/components/topbar/Topbar.jsx 25:42-50
 
 export default function Topbar() {
     return (
         <div className="topbar">
             <div className="wrapper">
                 <div className="topLeft">
-                    <a href = "#intro" className="logo">Início</a>
+                    <a href = "#intro" className="logo">Priscilla Cabral</a>                   
                     
+                </div>
+                <div className="topRight">
                     <div className="itemContainer">
-                        <MailIcon className="icon"/>
-                        <span></span>
-                    </div>
-                    <div className="itemContainer">
-                        <BookIcon className="icon"/>
+                        <BsJournalBookmarkFill className="icon"/>
                         <span>CRP 02/22596</span>
                     </div>
-                </div>
-                <h1>Priscilla Cabral</h1>
-                <div className="topRight">
-
+                    <div className="itemContainer">
+                        <BsWhatsapp className="icon"/>
+                        <span></span>
+                    </div>
                 </div>
             </div>
         </div>
