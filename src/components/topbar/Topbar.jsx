b4cae9738e75f7f8417b52072892dd2d7ee27243
Herbert Nícolas <hna@cin.ufpe.br>
2022-01-12 02:15:49 -0300

    having some issues on importing icons from mui

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,24 @@
 import "./topbar.scss"
+import {MailIcon} from '@mui/icons-material/Mail';      //ERROR in ./src/components/topbar/Topbar.jsx 25:42-50
+import {BookIcon} from '@mui/icons-material/Book';     //ERROR in ./src/components/topbar/Topbar.jsx 25:42-50
 
 export default function Topbar() {
     return (
         <div className="topbar">
             <div className="wrapper">
                 <div className="topLeft">
-                    <a href = "#intro">Home</a>
+                    <a href = "#intro" className="logo">Início</a>
+                    
+                    <div className="itemContainer">
+                        <MailIcon className="icon"/>
+                        <span></span>
+                    </div>
+                    <div className="itemContainer">
+                        <BookIcon className="icon"/>
+                        <span>CRP 02/22596</span>
+                    </div>
                 </div>
-                <h1>Olá!</h1>
+                <h1>Priscilla Cabral</h1>
                 <div className="topRight">
 
                 </div>
