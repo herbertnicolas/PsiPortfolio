256cbd70bf4ed47adad906002409645816d98ff6
Herbert Nícolas <hna@cin.ufpe.br>
2022-01-11 22:12:03 -0300

    added wrapper on top, home button, sections and some scrollbar effects

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,7 +3,15 @@ import "./topbar.scss"
 export default function Topbar() {
     return (
         <div className="topbar">
-            
+            <div className="wrapper">
+                <div className="topLeft">
+                    <a href = "#intro">Home</a>
+                </div>
+                <h1>Olá!</h1>
+                <div className="topRight">
+
+                </div>
+            </div>
         </div>
     )
 }
