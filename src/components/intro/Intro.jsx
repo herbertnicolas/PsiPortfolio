7dc8f414907c159848d1d33d0b06bf62f0027ac1
Herbert Nícolas <hna@cin.ufpe.br>
2022-01-17 18:20:57 -0300

    added photo and fonts updated after client recommendations

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,13 @@ import "./intro.scss"
 export default function intro() {
     return (
         <div className="intro" id="intro">
-            
+            <div className="left">
+                <div className="imgContainer">
+                       <img src="assets/pri-psico-removebg-preview.png" alt=""></img> 
+                </div>    
+            </div>
+
+            <div className="right"></div>
         </div>
     )
 }
