e36b5a4deacafd4e071aaa403ce5e24ffcb8a830
Herbert Nícolas <hna@cin.ufpe.br>
2022-01-19 16:44:43 -0300

    logo structures centered, logo background added, logo fonts modified

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -9,7 +9,20 @@ export default function intro() {
                 </div>    
             </div>
 
-            <div className="right"></div>
+            <div className="right">
+                
+                <div className="wrapper">
+                    <img src="assets/logo-bg.png" alt=""></img>
+                    <div className="container"><h2>Psicóloga Clínica e Atenção Psicossocial</h2></div>
+                    
+                    <h1>Adna Priscilla Cabral.</h1>
+                    <h3>CRP 02/22596</h3>
+                </div>
+                <a href="#portfolio">
+                    <img src="assets/down.png" alt=""></img>
+                </a>
+              
+            </div>
         </div>
     )
 }
