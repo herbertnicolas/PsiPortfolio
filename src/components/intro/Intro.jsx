d27f9f19052223aa7c002f6ac97ce0d68d33ea3c
Herbert Nícolas <hna@cin.ufpe.br>
2022-02-10 02:13:30 -0300

    added textFlow in h2 with ityped extension

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,17 @@
 import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react"
 
-export default function intro() {
+export default function Intro() {
+
+    const textFlow = useRef();
+    useEffect(()=>{
+        init(textFlow.current, { 
+            showCursor: false, 
+            strings: ['Psicóloga Clínica', 'Atenção Psicossocial' ] 
+        });
+    },[]);
+
     return (
         <div className="intro" id="intro">
             <div className="left">
@@ -9,14 +20,14 @@ export default function intro() {
                 </div>    
             </div>
 
-            <div className="right">
-                
+            <div className="right"> 
                 <div className="wrapper">
                     <img src="assets/logo-bg-small.png" alt=""></img>
                     <div className="textContainer">
-                        <h2>Psicóloga Clínica e Atenção Psicossocial</h2>
-                        <h1>Adna Priscilla Cabral.</h1>
                         <h3>CRP 02/22596</h3>
+                        <h1>Adna Priscilla Cabral.</h1>
+                        
+                        <h2> <span ref={textFlow}>Psicóloga Clínica</span></h2>
                     </div>
                 </div>
                 <a href="#portfolio">
